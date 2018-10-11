86fddb7fab6b61ca43ba7ed3d98184f80ee75094
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2018-10-11 11:56:27 +0900

    rewrite bag2ply

--- bag2ply/main.cpp
+++ bag2ply/main.cpp
@@ -21,48 +21,50 @@ int main(int argc, char* argv[] ) {
     rs2::frameset frames;
     rs2::points points;
     rs2::pointcloud pc;
+    rs2::colorizer color_map;
 
-    cv::namedWindow("Depth", CV_WINDOW_AUTOSIZE);
-    while(true)
-    {
-        if(pipe.poll_for_frames(&frames))
-        {
+    cv::namedWindow("Depth", CV_WINDOW_NORMAL);
+    cv::namedWindow("Color", CV_WINDOW_NORMAL);
+
+    cv::resizeWindow("Depth", 640, 480);
+    cv::resizeWindow("Color", 640, 480);
+
+    while(true) {
+
+        if(pipe.poll_for_frames(&frames)) {
+
             auto frame = pipe.wait_for_frames();
             auto depth_frame = frame.get_depth_frame();
             auto color_frame = frame.get_color_frame();
 
 
             cv::Mat color_mat = cv::Mat(cv::Size(color_frame.get_width(), color_frame.get_height()), CV_8UC3, const_cast<void *>( color_frame.get_data()));
-            cv::Mat ir(cv::Size(depth_frame.get_width(), depth_frame.get_height()), CV_16SC1, (void *) depth_frame.get_data(), cv::Mat::AUTO_STEP);
+            cv::Mat depth_mat(cv::Size(depth_frame.get_width(), depth_frame.get_height()), CV_16SC1, (void *) depth_frame.get_data(), cv::Mat::AUTO_STEP);
 
             points = pc.calculate(depth_frame);
             pc.map_to(color_frame);
-            points.export_to_ply("test.ply",color_frame);
 
-            cv::cvtColor(color_mat,color_mat,CV_RGB2BGR);
-            ir.convertTo(ir, CV_8U, -255.0/10000.0, 255.0);
-            cv::equalizeHist(ir, ir);
+            depth_mat.convertTo(depth_mat, CV_8U, -255.0/10000.0, 255.0);
+            cv::equalizeHist(depth_mat, depth_mat);
             //cv::applyColorMap(ir, ir, cv::COLORMAP_JET);
 
-            cv::imshow("Depth", ir);
+            cv::imshow("Depth", depth_mat);
             cv::imshow("Color", color_mat);
-            key = cv::waitKey(0);
+            const int key = cv::waitKey(30);
 
-            if (key == 's')
-            {
+            if(key=='s') {
                 output_names = std::to_string(number)+".ply";
                 points.export_to_ply(output_names,color_frame);
                 number++;
                 std::cout << "save ply_file" << std::endl;
             }
-
-            if (key == 'q') break;
+            else if(key==27) {
+                cv::destroyAllWindows();
+                break;
+            }
         }
     }
-
     pipe.stop();
 
-
     return 0;
-
 }
